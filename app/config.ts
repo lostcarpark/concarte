a5854bea80726cdc6a4264aabe7be76c5b7e85d9
lostcarpark <james@lostcarpark.com>
2024-10-26 00:42:27 +0100

    Maps and config for Gaelcon.

--- app/config.ts
+++ app/config.ts
@@ -1,19 +1,14 @@
 import { Config } from "./config.types";
 
 const config: Config = {
-  eventName: "DemoCon",
+  eventName: "Gaelcon 36 (2024)",
   locale: "en",
   description: `
-    An interactive map of the DemoCon venue.
-
-    This is an example of how you can use ConCarte to create an interactive
-    map of your event. This map is for a fictional event called DemoCon, and
-    includes various rooms and areas that you might find at a convention.
-
-    In the real world, this would be a brief description of your event, and
-    would include a link to your website.
+    An interactive map of the Gaelcon map.
   `,
   attributions: [
+    "Map designed by [Jog Brogzin](https://www.jogbrogzin.com/)",
+    "Setup assistance by James Shields",
     // Used in the favicon
     "Direction signs icon by [Delapouite](https://delapouite.com/) under [CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)",
   ],
@@ -27,449 +22,166 @@ const config: Config = {
     disabled: "#cbd5e1",
   },
   map: {
-    src: "/map.png",
+    src: "/Gaelcon_36_map_4K.webp",
     rooms: [
       {
-        id: "silent-room",
-        label: "Silent room",
-        aliases: ["Jackfield Boardroom"],
+        id: "ticketdesk",
+        label: "Gaelcon Ticket Desk",
+        aliases: ["Tickets", "Information"],
         description: `
-          This is a room intended for sitting in silence, where no activities
-          are allowed (including reading or using your phone). This will be
-          available each day from 11am. Note, there may be noise leaking from
-          neighbouring rooms, and the aircon makes some noise, so do bring noise
-          cancelling headphones if you need complete silence.
+          Get your tickets here
         `,
         area: [
-          [202, 499],
-          [255, 499],
-          [255, 550],
-          [202, 550],
+          [1631, 2152],
+          [1760, 2073],
+          [1917, 2159],
+          [1786, 2239],
         ],
       },
       {
-        id: "pattingham",
-        label: "Pattingham",
-        aliases: ["Programme"],
+        id: "gaelconbar",
+        label: "Gaelcon Bar",
+        aliases: ["Bar", "Pub"],
         description: `
-          [Programme schedule](https://guide.example.co.uk/pattingham)
+          Grab some drinks
         `,
         area: [
-          [262, 470],
-          [363, 470],
-          [363, 550],
-          [262, 550],
+          [1772, 2392],
+          [2130, 2179],
+          [2558, 2405],
+          [2203, 2627],
         ],
       },
       {
-        id: "ops-help-desk",
-        label: "Ops Help Desk",
-        aliases: ["Beckbury 1", "Beckbury 2"],
+        id: "tradehall",
+        label: "Redwood Suite",
+        aliases: ["Tradehall", "Open Gaming", "Seminars", "Demos"],
         description: `
-          If you need help with anything, this is the place to go. We can help
-          with lost property, lost people, and any other issues you might have.
-          This is also where the volunteers desk is.
-          
-          # Opening hours:
-          
-          Friday: 9:30am - 11:00pm
-          
-          Saturday: 9:30am-11pm
-          
-          Sunday: 9:30am-midnight
-          
-          Monday: 9:30am-11pm
+          Redwood Suite A/B - Tradehall and Demos, Redwood C - Open Gaming and Seminars
         `,
         area: [
-          [270, 655],
-          [363, 655],
-          [363, 728],
-          [270, 728],
+          [1964, 2051],
+          [2368, 1817],
+          [2955, 2144],
+          [2556, 2384],
         ],
       },
       {
-        id: "newsletter",
-        label: "Newsletter",
-        aliases: ["Beckbury 3"],
+        id: "wargames",
+        label: "Fahrenheit Suite",
+        aliases: ["Wargames"],
         description: `
-          Office for the newsletter team. If you want to submit something to the
-          newsletter, email
-          [newsletter@example.org](mailto:newsletter@example.org).
+          Wargaming
         `,
         area: [
-          [371, 655],
-          [414, 655],
-          [414, 728],
-          [371, 728],
+          [1062, 1470],
+          [1200, 1386],
+          [1136, 1345],
+          [1246, 1281],
+          [1324, 1326],
+          [1361, 1301],
+          [1639, 1464],
+          [1338, 1626],
         ],
       },
       {
-        id: "quiet-activities",
-        label: "Quiet activities",
-        aliases: ["Beckbury 4"],
+        id: "larp",
+        label: "LARPs",
+        aliases: ["Rooms 9&10"],
         description: `
-          A place where you may do a jigsaw or read a book/electronic item, but
-          may not make phone calls.
+          Live Action Roleplaying Games
         `,
         area: [
-          [422, 655],
-          [462, 655],
-          [462, 728],
-          [422, 728],
+          [1181, 1225],
+          [1452, 1064],
+          [1595, 1143],
+          [1323, 1306],
         ],
       },
       {
-        id: "games",
-        label: "Games",
-        aliases: ["Ryton"],
+        id: "ops",
+        label: "Ops",
+        aliases: ["Committee Room", "Operations", "Gaelcon Trolls"],
         description: `
-          Play games with friends, or make new friends by joining a game. There
-          will be a selection of games available, or you can bring your own.
+          Here be caffeinated trolls
         `,
         area: [
-          [884, 500],
-          [942, 500],
-          [942, 550],
-          [884, 550],
+          [1615, 1057],
+          [1728, 990],
+          [1861, 1065],
+          [1750, 1131],
         ],
       },
       {
-        id: "toilets-ground-floor",
-        label: "Toilets (ground floor)",
-        aliases: [
-          "Male toilets",
-          "Female toilets",
-          "Accessible toilets",
-          "Disabled toilets",
-        ],
+        id: "boardgames1",
+        label: "Boardgames: Room 15/16",
+        aliases: ["Boardgames: Room 15/16"],
         description: `
-          Gender neutral toilets are available on the first floor.
+          More Boardgames in Room 18 too!
         `,
         area: [
-          [852, 344],
-          [1055, 344],
-          [1055, 382],
-          [852, 382],
+          [1981, 1114],
+          [2206, 984],
+          [2470, 1130],
+          [2249, 1261],
         ],
       },
       {
-        id: "stairs-ground",
-        label: "Stairs (ground floor)",
-        area: [
-          [311, 585],
-          [484, 585],
-          [484, 615],
-          [311, 615],
-        ],
-      },
-      {
-        id: "lift-small-ground",
-        label: "Lift (small, ground floor)",
+        id: "boardgames2",
+        label: "Boardgames: Room 18",
+        aliases: ["Boardgames: Room 18"],
         description: `
-          This lift is small and can only fit one wheelchair user at a time.
-          If you are able to, please take the stairs. If you need a larger lift,
-          please use the lift near the E4 entrance.
+          More Boardgames in Room 15 & 16 too!
         `,
         area: [
-          [590, 585],
-          [621, 585],
-          [621, 615],
-          [590, 615],
+          [3298, 895],
+          [3524, 763],
+          [3653, 834],
+          [3432, 965],
         ],
       },
       {
-        id: "lift-large-ground",
-        label: "Lift (large, ground floor)",
+        id: "fingal",
+        label: "Fingal Community Gaming",
+        aliases: ["Fingal Libraries"],
         description: `
-          You will need to ask a member of staff to call the lift for you.
+          Games run by Fingal Community Gaming
         `,
         area: [
-          [1123, 149],
-          [1193, 149],
-          [1193, 209],
-          [1123, 209],
+          [2864, 1017],
+          [3031, 919],
+          [3135, 980],
+          [2969, 1079],
         ],
       },
       {
-        id: "e1",
-        label: "E1",
-        aliases: ["Entrance 1", "Car park"],
+        id: "quietplace",
+        label: "A Quiet Place",
+        aliases: ["Sensory Room", "Quiet Room", "Snug"],
         description: `
-          # Opening times
-          
-          Friday: 9:30am - 11:00pm
-          
-          Saturday: 9:30am-11pm
-          
-          Sunday: 9:30am-midnight
-          
-          Monday: 9:30am-11pm
+          Somewhere quiet to recharge from con fatigue
         `,
         area: [
-          [1008, 690],
-          [1170, 690],
-          [1170, 808],
-          [1008, 808],
+          [3068, 863],
+          [3193, 792],
+          [3274, 848],
+          [3151, 920],
         ],
       },
       {
-        id: "e2",
-        label: "E2",
-        aliases: ["Entrance 2"],
+        id: "rpgs",
+        label: "RPGs: Rooms 20-24",
+        aliases: ["Roleplaying Games"],
         description: `
-          # Opening times
-          
-          Friday: 9:30am - 11:00pm
-          
-          Saturday: 9:30am-11pm
-          
-          Sunday: 9:30am-midnight
-          
-          Monday: 9:30am-11pm
+          Adventure Time!
         `,
         area: [
-          [470, 654],
-          [632, 654],
-          [632, 808],
-          [470, 808],
-        ],
-      },
-      {
-        id: "e3",
-        label: "E3",
-        aliases: ["Entrance 3"],
-        area: [
-          [44, 706],
-          [233, 655],
-          [233, 808],
-          [44, 808],
-        ],
-      },
-      {
-        id: "e4",
-        label: "E4",
-        aliases: ["Entrance 4", "Hotels"],
-        description: `
-          # Opening times
-          
-          Friday: 9:30am - 11:00pm
-          
-          Saturday: 9:30am-11pm
-          
-          Sunday: 9:30am-midnight
-          
-          Monday: 9:30am-11pm
-        `,
-        area: [
-          [1008, 35],
-          [1170, 35],
-          [1170, 150],
-          [1008, 150],
-        ],
-      },
-      {
-        id: "ironbridge",
-        label: "Ironbridge",
-        aliases: ["Programme"],
-        description: `
-          [Programme schedule](https://guide.example.co.uk/ironbridge)
-        `,
-        area: [
-          [1321, 363],
-          [1457, 363],
-          [1457, 613],
-          [1332, 647],
-          [1321, 565],
-        ],
-      },
-      {
-        id: "dealers",
-        label: "Dealers & Fan Tables",
-        description: `
-          A selection of dealers and fan tables will be available for you to
-          browse. Please note that some dealers may only accept cash.
-          
-          # Opening times
-          
-          Friday: 2pm-7pm
-          
-          Saturday: 10am-6pm
-          
-          Sunday: 10am-6pm
-          
-          Monday: 10am-2pm
-        `,
-        area: [
-          [1465, 363],
-          [1576, 363],
-          [1576, 426],
-          [1608, 426],
-          [1608, 544],
-          [1465, 581],
-        ],
-      },
-      {
-        id: "childcare",
-        label: "Childcare",
-        description: `
-          Childcare must have been pre-booked. If you have not pre-booked, you
-          will not be able to use this service.
-        `,
-        area: [
-          [1576, 363],
-          [1576, 426],
-          [1639, 426],
-          [1639, 363],
-        ],
-      },
-      {
-        id: "art-show",
-        label: "Art show",
-        description: `
-          The art show will be open from 10am-6pm each day. Please note that
-          some pieces may be for sale.
-          
-          # Opening times
-          
-          Friday: Preview 4pm-5pm, sales open 5pm-7pm
-          
-          Saturday: 10am-6pm
-          
-          Sunday: 10am-5pm (collection of purchased art, 4pm-6:30pm)
-        `,
-        area: [
-          [1639, 363],
-          [1752, 363],
-          [1752, 505],
-          [1608, 544],
-          [1608, 426],
-          [1639, 426],
-        ],
-      },
-      {
-        id: "toilets-first-floor",
-        label: "Toilets (first floor)",
-        aliases: ["Urinals", "Without urinals", "Accessible"],
-        description: `
-          Gender neutral toilets with and without urinals.
-        `,
-        area: [
-          [1760, 434],
-          [1867, 434],
-          [1867, 504],
-          [1760, 504],
-        ],
-      },
-      {
-        id: "green-room",
-        label: "Green room",
-        description: `
-          Programme participants should arrive at the green room 15 minutes
-          before their item starts. Here you will meet with your fellow
-          panellists (both in-person and online) and discuss the item before it
-          starts. You will also be offered a complementary drink.
-        `,
-        area: [
-          [1584, 674],
-          [1677, 674],
-          [1677, 747],
-          [1584, 747],
-        ],
-      },
-      {
-        id: "Wenlock",
-        label: "Wenlock",
-        aliases: ["Programme"],
-        description: `
-          [Programme schedule](https://guide.example.co.uk/wenlock)
-        `,
-        area: [
-          [1685, 674],
-          [1776, 674],
-          [1776, 747],
-          [1685, 747],
-        ],
-      },
-      {
-        id: "stairs-first",
-        label: "Stairs (first floor)",
-        area: [
-          [1754, 588],
-          [1933, 588],
-          [1933, 618],
-          [1754, 618],
-        ],
-      },
-      {
-        id: "lift-small-first",
-        label: "Lift (small, first floor)",
-        description: `
-          This lift is small and can only fit one wheelchair user at a time. If
-          you are able to, please take the stairs. If you need a larger lift,
-          please use the lift near the E4 entrance.
-        `,
-        area: [
-          [1993, 602],
-          [2024, 593],
-          [2024, 623],
-          [1993, 623],
-        ],
-      },
-      {
-        id: "coalport",
-        label: "Coalport",
-        aliases: ["Programme"],
-        description: `
-          [Programme schedule](https://guide.example.co.uk/coalport)
-        `,
-        area: [
-          [1875, 434],
-          [1977, 434],
-          [1977, 504],
-          [1875, 504],
-        ],
-      },
-      {
-        id: "gallary",
-        label: "Gallary",
-        aliases: ["Food & Drink", "Social space"],
-        description: `
-          A place to sit and chat with friends, or to grab a bite to eat. There
-          will be a selection of food and drink available, including vegan and
-          gluten free options.
-        `,
-        area: [
-          [2029, 363],
-          [2288, 363],
-          [2288, 499],
-          [2029, 499],
-        ],
-      },
-      {
-        id: "atcham",
-        label: "Atcham",
-        aliases: ["Programme"],
-        description: `
-          [Programme schedule](https://guide.example.co.uk/atcham)
-        `,
-        area: [
-          [2296, 363],
-          [2397, 363],
-          [2397, 493],
-          [2296, 493],
-        ],
-      },
-      {
-        id: "lift-large-first",
-        label: "Lift (large, first floor)",
-        description: `
-          You will need to ask a member of staff to call the lift for you.
-        `,
-        area: [
-          [2405, 320],
-          [2468, 320],
-          [2468, 369],
-          [2405, 369],
+          [2810, 709],
+          [3124, 533],
+          [3510, 755],
+          [3372, 837],
+          [3239, 751],
+          [3049, 862],
         ],
       },
     ],
